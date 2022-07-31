a0998c475d51e547ae071f4683b401048bd06629
TuhinSa <92089255+TuhinSa@users.noreply.github.com>
2022-07-31 12:30:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+    
+video = createCapture(VIDEO);
+video.size(480, 380);
+video.hide();
+}
+function start(){
+    objectDetector  = ml5.objectDetector("cocossd", modelLoaded)
+document.getElementById("status").innerHTML = "status: Detecting objects";
+finding_object = document.getElementById("input").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    Status = true
+}
+function draw(){
+    image(video,0,0, 480,380);
+}
